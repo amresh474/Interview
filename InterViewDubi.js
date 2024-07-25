6f0e4394b5a6f21c60bb2af8d3cda8812d1b95a4
amreshkumar474 <60948058+amreshkumar474@users.noreply.github.com>
2024-07-25 17:01:35 +0530

    Add files via upload

---
+++ InterViewDubi.js
@@ -0,0 +1,63 @@
+
+// Node js Rest Api 
+const express = require('express');
+const auth = require('./auth');
+const app = express();
+app.use(express.json);
+
+const port = process.env.PORT || 4000
+
+app.get('/',auth.isAuthorized,(req,res)=>{
+
+    const {a,b} = req.params;
+    if(a || b || !Number.isNaN(a)  || !Number.isNaN(b) ){
+       res.status(400).send(' Invalid input ') 
+    }
+     res.status(200).send(Number(a)+Number(b));
+
+})
+
+app.listen(port,()=>console.log(`listening on port ${port}`));
+
+
+// React js code 
+
+import { useEffect, useState } from "react";
+
+function app(params) {
+
+    const url = 'https://dummjson.com/productsdummyjson.com';
+
+    const [products,setProducts]=useState([]);
+    const [loading ,setLoading]=useState(false);
+    
+    useEffect(()=>{
+        setLoading(true);
+        fetch(url).then(res => res.json()).then(data => setProducts(data))
+        .finally(()=>{setLoading(false)});
+    },[])
+
+    return(
+        <div className="App">
+            {loading? (<div> Loading...</div>):
+        (<>
+        <h1> Product details </h1>
+        <table border={1}>
+            <tr>
+                <th>Product Name </th>
+                <th> Description </th>
+                <th>Image</th>
+            </tr>
+            {products && products.map(product =>{
+                return (
+                <tr key={product.id}>
+                    <td>{product.name}</td>
+                    <td>{product.discription}</td>
+                    <td> <img src={product.url} alt="image"/></td>
+                </tr>
+                )})}
+        </table>
+        </>)}
+        </div>
+    )
+}
